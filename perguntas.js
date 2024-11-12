fd741c6f99e63f80fd77865d0ec296eae5fcec95
bibiasm <00001095692343sp@al.educacao.sp.gov.br>
2024-11-12 00:53:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Gilmore Girls',
+    'Quem é Jess?',
+    'É o sobrinho de Luke, que é namorado da Lorelai, além de ser o personagem mais lindo e ex da Rory!'
+)
+
+criaCartao(
+    'Gossip Girl',
+    'Quem era a "Garota do Blog"?',
+    'A "Garota do Blog" na verdade era o Dan Humphrey!'
+)
+
+criaCartao(
+    'One Tree Hill',
+    'Quem é Nathan Scott?',
+    'O homem mais lindo da série, jogador número 23 de basquete no time de Tree Hill, apaga até o Lucas, (Chad M. Murray), que é o número 3!'
+)
+
+criaCartao(
+    'Friends',
+    'Qual é o casal considerado "os pais"?',
+    'Monica e Chandler!!!'
+)
